65e71764f8d70d1e11a15ae932501eff68146211
Cecelia Bloom <crbloom01@gmail.com>
2024-01-30 15:18:24 -0500

    finished

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const searchBtnElement= document.getElementById("submitSearch");
+
+const searchInput = document.getElementById("searchWord");
+
+const imageElement = document.querySelector("#imageContainer > img");
+
+const feedback = document.getElementById("p");
+
+const baseUrl="https://api.giphy.com/v1/gifs/translate";
+
+const key="niObP31iC56GTaKoSZnM8BbO0ULXGs80";
+
+
+
+searchBtnElement.addEventListener("click",()=>{
+   fetch(`${baseUrl}?api_key=${key}&s=${searchInput.value}`)
+   
+   .then((response) => response.json())
+    .then((res) => {
+      imageElement.src = res.data.images.original.url;
+      searchInput.value = "";  
+   })
+   .catch((err)=>{
+        feedback.textContent = err.message;
+   })
+});
+
+
+
+
+
+
+
+
